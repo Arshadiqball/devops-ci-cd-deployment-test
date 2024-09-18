e65d853a2e478519f10e52eaf37adcd95ccd2949
Arshad Iqbal <arshadiqbal.d@gmail.com>
2024-09-18 18:38:38 +0500

    fix

--- db.php
+++ db.php
@@ -1,4 +1,5 @@
 <?php
+
 // Simulate a database connection and fetching posts
 function get_all_posts()
 {
@@ -7,3 +8,5 @@ function get_all_posts()
         ['title' => 'Second Post', 'content' => 'This is the content of the second post.']
     ];
 }
+
+// No closing PHP tag
