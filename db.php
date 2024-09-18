57bd84794247b6dc14163588a81133a624108818
Arshad Iqbal <arshadiqbal.d@gmail.com>
2024-09-18 18:37:02 +0500

    fix

--- db.php
+++ db.php
@@ -1,10 +1,9 @@
-
 <?php
 // Simulate a database connection and fetching posts
-function get_all_posts() {
+function get_all_posts()
+{
     return [
         ['title' => 'First Post', 'content' => 'This is the content of the first post.'],
         ['title' => 'Second Post', 'content' => 'This is the content of the second post.']
     ];
 }
-?>
